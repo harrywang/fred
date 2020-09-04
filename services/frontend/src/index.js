77a2c22f505ef0a938e465c8b11bf438599da3b4
zhuojg <zhuojg1519@outlook.com>
2020-09-04 10:55:35 +0800

    feature: add ant design and ant design landing

--- services/frontend/src/index.js
+++ services/frontend/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App.jsx';
 
 ReactDOM.render(
-  (
-    <Router>
-      <App />
-    </Router>
-  ),
+  <App />,
   document.getElementById('root')
 );
