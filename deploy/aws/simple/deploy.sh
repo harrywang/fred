fd3b04d5e60fccdaff2451b185b42461779d5ebe
zhuojg <zhuojg1519@outlook.com>
2020-11-17 15:36:42 +0800

    fix: now post_build should work

---
+++ deploy/aws/simple/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+JQ="jq --raw-output --exit-status"
+
+configure_aws_cli() {
+  aws --version
+  aws configure set default.region us-east-1
+  aws configure set default.output json
+  echo "AWS Configured!"
+}
+
+register_definition() {
+  if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" | $JQ '.taskDefinition.taskDefinitionArn'); then
+    echo "Revision: $revision"
+  else
+    echo "Failed to register task definition"
+    return 1
+  fi
+}
+
+
+update_service() {
+  if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+    echo "Error updating service."
+    return 1
+  fi
+}
+
+
+deploy_cluster() {
+
+  cluster="fred-cluster"
+
+  # backend
+  service="fred-service"
+  template="ecs_fred_task_definition.json"
+  task_template=$(cat "deploy/aws/simple/$template")
+  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
+  echo "$task_def"
+  register_definition
+  update_service
+}
+
+configure_aws_cli
+deploy_cluster
